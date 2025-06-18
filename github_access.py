7bdb5d8a64a6428ea8bfcb51a27806d6d7e5fefc
Joshua Reyes <Joshreyes@gmail.com>
2025-06-18 10:27:10 +0100

    Update github_access.py

--- github_access.py
+++ github_access.py
@@ -7,7 +7,7 @@ def download_github_files(repo_name, branch="main", local_dir="./", token=None):
     if token:
         git = Github(token)
     else:
-        git = Github()  # Unauthenticated (60 req/hr limit)
+        git = Github()  #unauthenticated (60 req/hr limit)
 
     repo = git.get_repo(repo_name)
     contents = repo.get_contents("", ref=branch)
@@ -22,12 +22,18 @@ def download_recursive(contents_list, repo, branch, local_dir):
         contents_list = [contents_list]
 
     for content_file in contents_list:
+
+        ##directory recusion
         if content_file.type == "dir":
+            
             new_contents = repo.get_contents(content_file.path, ref=branch)
             download_recursive(new_contents, repo, branch, local_dir)
+    
+        ##file download
         elif content_file.type == "file":
             save_path = os.path.join(local_dir, content_file.path)
             os.makedirs(os.path.dirname(save_path), exist_ok=True)
+            
             try:
                 urllib.request.urlretrieve(content_file.download_url, save_path)
                 print(f"Downloaded: {save_path}")
