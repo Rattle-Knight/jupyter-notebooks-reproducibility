a1478a858d63f86cbc4e06789ca07932cab21fb8
Joshua Reyes <Joshreyes@gmail.com>
2025-08-07 16:33:40 +0100

    Update notebook_run.py

--- notebook_run.py
+++ notebook_run.py
@@ -13,14 +13,20 @@ def access_notebook(notebookdir,env_name,conda_path):
     print(config.stage)
 
     # this runs the notebook before the reporting
-    result = config.command([
+    result = subprocess.run([
         conda_path, "run", "-n", env_name,
         "jupyter", "nbconvert",
         "--to", "notebook",
         "--execute", "--inplace",
         "--ExecutePreprocessor.allow_errors=True",
+        "--ExecutePreprocessor.timeout=600",
         notebook_path
     ], capture_output=True, text=True)
+
+    if result.returncode != 0:
+         print("Notebook execution exited with error code:", result.returncode)
+    else:
+        print("Notebook executed successfully.")
 
 
 
